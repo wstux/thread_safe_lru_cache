5f4525a55204d252019e100080e173f579852c47
Chistyakov Alexander <wstux1@gmail.com>
2023-06-19 23:42:23 +0300

    + tests: #4 Tests refactoring;

--- tests/ut_lru_cache.cpp
+++ tests/ut_lru_cache.cpp
@@ -22,17 +22,136 @@
  * THE SOFTWARE.
  */
 
+#include <atomic>
+#include <random>
+#include <thread>
+
 #include <testing/testdefs.h>
 
 #include "lru/lru_cache.h"
+#include "lru/thread_safe_lru_cache.h"
 
-TEST(cache, emplace)
+namespace {
+
+////////////////////////////////////////////////////////////////////////////////
+// class thread_safe_cache_fixture
+
+class thread_safe_cache_fixture : public ::testing::Test
 {
-    using lru_cache = ::wstux::lru::lru_cache<size_t, std::string>;
+    using base = ::testing::Test;
+    using test_data_vector = std::vector<size_t>;
 
-    lru_cache cache(10);
+public:
+    virtual void SetUp() override
+    {
+        base::SetUp();
 
-    lru_cache::value_type val;
+        m_run_threads = threads_count();
+
+        m_test_data.reserve(count());
+        for (size_t i = 0; i < count(); ++i) {
+            m_test_data.emplace_back(i);
+        }
+
+        m_hit_count = {0};
+        m_total_count = {0};
+    }
+
+    void join_threads() { for (std::thread& t : m_threads) { t.join(); } }
+
+    template<typename TCache>
+    void init_threads(TCache& c)
+    {
+        m_threads.clear();
+        m_threads.reserve(threads_count());
+        for (size_t i = 0; i < threads_count(); ++i) {
+            m_threads.emplace_back([this, &c]() -> void { thread_main(c); });
+        }
+    }
+
+    size_t count() const { return 100; }
+
+    bool is_threads_started() const { return (m_run_threads == 0); }
+
+    void start() { m_is_start = true; }
+
+    size_t threads_count() const { return 5; }
+
+    size_t hit_count() const { return m_hit_count; }
+    size_t total_count() const { return m_total_count; }
+
+protected:
+    template<typename TCache>
+    void thread_main(TCache& cache)
+    {
+        const test_data_vector& td = m_test_data;
+        const std::string val = "";
+
+        std::mt19937 gen(std::hash<std::thread::id>()(std::this_thread::get_id()));
+        std::uniform_int_distribution<size_t> rand(0, m_test_data.size() - 1);
+
+        --m_run_threads;
+        while (! m_is_start) {}
+
+        for (size_t i = 0; i < 10 * td.size(); ++i) {
+            const typename TCache::key_type& key = td[rand(gen)];
+            typename TCache::value_type val;
+            if (! cache.find(key, val)) {
+                cache.insert(key, key);
+            } else {
+                EXPECT_TRUE(key == val) << "key('" << key << "') != value('" << val << "'";
+                ++m_hit_count;
+            }
+            ++m_total_count;
+        }
+    }
+
+private:
+    std::atomic_bool m_is_start = {false};
+    std::atomic_size_t m_run_threads = {0};
+
+    std::vector<std::thread> m_threads;
+    test_data_vector m_test_data;
+
+    std::atomic_size_t m_hit_count;
+    std::atomic_size_t m_total_count;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// class cache_fixture
+
+template<typename TLruCache>
+class cache_fixture : public ::testing::Test
+{};
+
+struct lru_cache
+{
+    using cache = ::wstux::lru::lru_cache<size_t, std::string>;
+
+    static cache create() { return cache(10); }
+};
+
+struct thread_safe_lru_cache
+{
+    using cache = ::wstux::lru::thread_safe_lru_cache<size_t, std::string>;
+
+    static cache create() { return cache(10, 2); }
+};
+
+using lru_types = testing::Types<lru_cache,
+                                 thread_safe_lru_cache>;
+TYPED_TEST_SUITE(cache_fixture, lru_types);
+
+} // <anonymous> namespace
+
+TYPED_TEST(cache_fixture, emplace)
+{
+    using cache_type = TypeParam;
+    using lru_cache = typename cache_type::cache;
+
+    lru_cache cache = cache_type::create();
+
+    typename lru_cache::value_type val;
     EXPECT_FALSE(cache.find(0, val));
 
     EXPECT_TRUE(cache.emplace(0, 4, 'b'));
@@ -40,13 +159,14 @@ TEST(cache, emplace)
     EXPECT_TRUE(val == "bbbb") << val;
 }
 
-TEST(cache, insert)
+TYPED_TEST(cache_fixture, insert)
 {
-    using lru_cache = ::wstux::lru::lru_cache<size_t, std::string>;
+    using cache_type = TypeParam;
+    using lru_cache = typename cache_type::cache;
 
-    lru_cache cache(10);
+    lru_cache cache = cache_type::create();
 
-    lru_cache::value_type val;
+    typename lru_cache::value_type val;
     EXPECT_FALSE(cache.find(0, val));
 
     EXPECT_TRUE(cache.insert(0, std::string(4, 'b')));
@@ -54,28 +174,30 @@ TEST(cache, insert)
     EXPECT_TRUE(val == "bbbb") << val;
 }
 
-TEST(cache, empty)
+TYPED_TEST(cache_fixture, empty)
 {
-    using lru_cache = ::wstux::lru::lru_cache<size_t, std::string>;
+    using cache_type = TypeParam;
+    using lru_cache = typename cache_type::cache;
 
-    lru_cache cache(10);
+    lru_cache cache = cache_type::create();
 
     EXPECT_TRUE(cache.empty());
     cache.emplace(0, 4, 'b');
     EXPECT_FALSE(cache.empty());
 }
 
-TEST(cache, erase)
+TYPED_TEST(cache_fixture, erase)
 {
-    using lru_cache = ::wstux::lru::lru_cache<size_t, std::string>;
+    using cache_type = TypeParam;
+    using lru_cache = typename cache_type::cache;
 
-    lru_cache cache(10);
+    lru_cache cache = cache_type::create();
 
     EXPECT_TRUE(cache.empty());
     EXPECT_TRUE(cache.insert(0, std::string(4, 'b')));
     EXPECT_FALSE(cache.empty());
 
-    lru_cache::value_type val;
+    typename lru_cache::value_type val;
     EXPECT_TRUE(cache.find(0, val));
 
     cache.erase(0);
@@ -83,11 +205,12 @@ TEST(cache, erase)
     EXPECT_FALSE(cache.find(0, val));
 }
 
-TEST(cache, size)
+TYPED_TEST(cache_fixture, size)
 {
-    using lru_cache = ::wstux::lru::lru_cache<size_t, std::string>;
+    using cache_type = TypeParam;
+    using lru_cache = typename cache_type::cache;
 
-    lru_cache cache(10);
+    lru_cache cache = cache_type::create();
 
     EXPECT_TRUE(cache.size() == 0);
     cache.emplace(0, 4, 'b');
@@ -96,13 +219,14 @@ TEST(cache, size)
     EXPECT_TRUE(cache.size() == 0);
 }
 
-TEST(cache, update)
+TYPED_TEST(cache_fixture, update)
 {
-    using lru_cache = ::wstux::lru::lru_cache<size_t, std::string>;
+    using cache_type = TypeParam;
+    using lru_cache = typename cache_type::cache;
 
-    lru_cache cache(10);
+    lru_cache cache = cache_type::create();
 
-    lru_cache::value_type val;
+    typename lru_cache::value_type val;
     EXPECT_FALSE(cache.find(0, val));
     EXPECT_FALSE(cache.find(1, val));
 
@@ -119,7 +243,7 @@ TEST(cache, update)
     EXPECT_TRUE(val == "ccc") << val;
 }
 
-TEST(cache, hit)
+TEST(lru_cache, hit)
 {
     using lru_cache = ::wstux::lru::lru_cache<size_t, size_t>;
     using test_data_vector = std::vector<lru_cache::key_type>;
@@ -149,7 +273,28 @@ TEST(cache, hit)
         << "hit_count = " << hit_count << "; total_count = " << total_count;
 }
 
+TEST_F(thread_safe_cache_fixture, hit)
+{
+    using lru_cache = ::wstux::lru::thread_safe_lru_cache<size_t, size_t>;
+
+    lru_cache cache(30, threads_count());
+    init_threads(cache);
+
+    // Waiting until all threads have started.
+    while (! is_threads_started()) {}
+    // Start work.
+    start();
+    // Finish threads.
+    join_threads();
+
+    const size_t actual_total_count = 10 * threads_count() * count();
+    EXPECT_TRUE(actual_total_count == total_count()) << "total_count = " << total_count();
+    EXPECT_TRUE(hit_count() > 0) << "hit_count = " << hit_count();
+    EXPECT_TRUE(hit_count() < total_count()) << "hit_count = " << hit_count();
+}
+
 int main(int /*argc*/, char** /*argv*/)
 {
     return RUN_ALL_TESTS();
 }
+
