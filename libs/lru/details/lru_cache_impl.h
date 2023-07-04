51eb76a7c39f1f3622be608cd19b471b41634350
Chistyakov Alexander <wstux1@gmail.com>
2023-07-04 20:44:44 +0300

    + libs/lru: #23 Moved specified code for cache with standard containers;

--- libs/lru/details/lru_cache_impl.h
+++ libs/lru/details/lru_cache_impl.h
@@ -26,8 +26,8 @@
 #define _LRU_CACHE_LRU_CACHE_IMPL_H
 
 #include <functional>
-#include <list>
-#include <unordered_map>
+
+#include "lru/details/std_base_lru_cache.h"
 
 namespace wstux {
 namespace lru {
@@ -35,51 +35,38 @@ namespace lru {
 /// \todo   Add allocator as template parameter.
 template<typename TKey, typename TValue,
          class THash = std::hash<TKey>, class TKeyEqual = std::equal_to<TKey>>
-class lru_cache
+class lru_cache : protected details::base_lru_cache<TKey, TValue, THash, TKeyEqual>
 {
-public:
-    typedef TKey                key_type;
-    typedef TValue              value_type;
-    typedef size_t              size_type;
-    typedef THash               hasher;
-    typedef TKeyEqual           key_equal;
-    typedef value_type&         reference;
-    typedef const value_type&   const_reference;
-    typedef value_type*         pointer;
-    typedef const value_type*   const_pointer;
+private:
+    typedef details::base_lru_cache<TKey, TValue, THash, TKeyEqual>     base;
 
-    explicit lru_cache(size_t capacity)
-        : m_capacity(capacity)
-        , m_size(0)
-    {
-        /*
-         * In the update(...) function, the hash table is first inserted into
-         * the hash table, after which the size is checked, so a situation may
-         * arise when the table stores (capacity + 1) elements. Because of this
-         * behavior, recalculation and reallocation of the table can occur, so
-         * the table is reserved for (capacity + 1) elements to prevent
-         * rehashing of the table hash.
-         */
-        m_cache.reserve(m_capacity + 1);
-    }
+public:
+    typedef typename base::key_type          key_type;
+    typedef typename base::value_type        value_type;
+    typedef typename base::size_type         size_type;
+    typedef typename base::hasher            hasher;
+    typedef typename base::key_equal         key_equal;
+    typedef typename base::reference         reference;
+    typedef typename base::const_reference   const_reference;
+    typedef typename base::pointer           pointer;
+    typedef typename base::const_pointer     const_pointer;
+
+    explicit lru_cache(size_type capacity)
+        : base(capacity)
+    {}
 
     lru_cache(const lru_cache&) = delete;
     lru_cache& operator=(const lru_cache&) = delete;
 
-    size_type capacity() const { return m_capacity; }
+    size_type capacity() const { return base::capacity(); }
 
-    void clear()
-    {
-        m_size = 0;
-        m_cache.clear();
-        m_lru_list.clear();
-    }
+    void clear() { base::clear(); }
 
     bool contains(const key_type& key)
     {
-        typename _lru_map_t::iterator it = m_cache.find(key);
-        if (it != m_cache.end()) {
-            move_to_top(m_lru_list, it->second.lru_it);
+        typename _hash_table_t::iterator it = base::find_in_tbl(key);
+        if (base::is_find(it)) {
+            base::move_to_top(it);
             return true;
         }
         return false;
@@ -88,132 +75,75 @@ public:
     template<typename... TArgs>
     bool emplace(const key_type& key, TArgs&&... args)
     {
-        typename _lru_map_t::iterator it = m_cache.find(key);
-        if (it != m_cache.end()) {
-            move_to_top(m_lru_list, it->second.lru_it);
+        typename _hash_table_t::iterator it = base::find_in_tbl(key);
+        if (base::is_find(it)) {
+            base::move_to_top(it);
             return false;
         }
 
-        insert_to_cache(key, std::forward<TArgs>(args)...);
+        base::insert(key, std::forward<TArgs>(args)...);
         return true;
     }
 
-    bool empty() const { return (size() == 0); }
+    bool empty() const { return (base::size() == 0); }
 
-    void erase(const key_type& key)
-    {
-        typename _lru_map_t::iterator it = m_cache.find(key);
-        if (it != m_cache.end()) {
-            m_lru_list.erase(it->second.lru_it);
-            m_cache.erase(it);
-            --m_size;
-        }
-    }
+    void erase(const key_type& key) { base::erase(key); }
 
     bool find(const key_type& key, value_type& result)
     {
-        typename _lru_map_t::iterator it = m_cache.find(key);
-        if (it == m_cache.end()) {
-            return false;
+        typename _hash_table_t::iterator it = base::find_in_tbl(key);
+        if (base::is_find(it)) {
+            base::move_to_top(it);
+            base::load(it, result);
+            return true;
         }
 
-        move_to_top(m_lru_list, it->second.lru_it);
-        result = it->second.value;
-        return true;
+        return false;
     }
 
     bool insert(const key_type& key, const value_type& val)
     {
-        typename _lru_map_t::iterator it = m_cache.find(key);
-        if (it != m_cache.end()) {
-            move_to_top(m_lru_list, it->second.lru_it);
+        typename _hash_table_t::iterator it = base::find_in_tbl(key);
+        if (base::is_find(it)) {
+            base::move_to_top(it);
             return false;
         }
 
-        insert_to_cache(key, val);
+        base::insert(key, val);
         return true;
     }
 
-    void reserve(size_type new_capacity)
-    {
-        m_capacity = new_capacity;
-        m_cache.reserve(m_capacity);
-    }
+    void reserve(size_type new_capacity) { base::reserve(new_capacity); }
 
-    size_type size() const { return m_size; }
+    size_type size() const { return base::size(); }
 
     void update(const key_type& key, const value_type& val)
     {
-        typename _lru_map_t::iterator it = m_cache.find(key);
-        if (it != m_cache.end()) {
-            it->second.value = val;
-            move_to_top(m_lru_list, it->second.lru_it);
+        typename _hash_table_t::iterator it = base::find_in_tbl(key);
+        if (base::is_find(it)) {
+            base::store(it, value_type(val));
+            base::move_to_top(it);
             return;
         }
 
-        insert_to_cache(key, val);
+        base::insert(key, val);
     }
 
     void update(const key_type& key, value_type&& val)
     {
-        typename _lru_map_t::iterator it = m_cache.find(key);
-        if (it != m_cache.end()) {
-            it->second.value = std::move(val);
-            move_to_top(m_lru_list, it->second.lru_it);
+        typename _hash_table_t::iterator it = base::find_in_tbl(key);
+        if (base::is_find(it)) {
+            base::store(it, std::move(val));
+            base::move_to_top(it);
             return;
         }
 
-        insert_to_cache(key, std::move(val));
+        base::insert(key, std::move(val));
     }
 
 private:
-    struct _map_value_t
-    {
-        template<typename... TArgs>
-        explicit _map_value_t(TArgs&&... args)
-            : value(std::forward<TArgs>(args)...)
-        {}
-
-        value_type value;
-        typename std::list<key_type>::iterator lru_it;
-    };
-
-    /// \todo   Remove key duplicate.
-    using _lru_list_t = std::list<key_type>;
-    using _lru_map_t = std::unordered_map<key_type, _map_value_t, hasher, key_equal>;
-
-private:
-    template<typename... TArgs>
-    inline void insert_to_cache(const key_type& key, TArgs&&... args)
-    {
-        if (m_size >= m_capacity) {
-            m_cache.erase(m_lru_list.front());
-            m_lru_list.front() = key;
-            std::pair<typename _lru_map_t::iterator, bool> rc =
-                m_cache.emplace(key, _map_value_t(std::forward<TArgs>(args)...));
-            rc.first->second.lru_it = m_lru_list.begin();
-            move_to_top(m_lru_list, rc.first->second.lru_it);
-        } else {
-            typename _lru_list_t::iterator it = m_lru_list.emplace(m_lru_list.end(), key);
-            std::pair<typename _lru_map_t::iterator, bool> rc =
-                m_cache.emplace(key, _map_value_t(std::forward<TArgs>(args)...));
-            rc.first->second.lru_it = it;
-            ++m_size;
-        }
-    }
-
-    template<typename TIterator>
-    inline static void move_to_top(_lru_list_t& list, TIterator& it)
-    {
-        list.splice(list.end(), list, it);
-    }
-
-private:
-    size_t m_capacity;
-
-    size_t m_size;
-    _lru_map_t m_cache;
-    _lru_list_t m_lru_list;
+    typedef typename base::_lru_list_t          _lru_list_t;
+    typedef typename base::_hash_table_t        _hash_table_t;
 };
 
 } // namespace lru
