3d0ff4ae66a05a8b69abc6b54776d3bd6a7f525a
Chistyakov Alexander <wstux1@gmail.com>
2024-10-26 12:39:08 +0300

    * cmake: #29 Updated build system;

--- cmake/build_utils.cmake
+++ cmake/build_utils.cmake
@@ -24,19 +24,26 @@
 # Functions
 ################################################################################
 
+function(_get_qualifier TARGET_NAME RESULT)
+    set(${RESULT} PRIVATE PARENT_SCOPE)
+    get_target_property(_type ${TARGET_NAME} TYPE)
+    if(_type STREQUAL "INTERFACE_LIBRARY")
+        set(${RESULT} INTERFACE PARENT_SCOPE)
+    endif()
+endfunction()
+
 function(_configure_target TARGET_NAME)
     if (NOT ${TARGET_NAME}_INCLUDE_DIR)
-        get_target_property(_cur_target_type ${TARGET_NAME} TYPE)
-        if(NOT _cur_target_type STREQUAL "INTERFACE_LIBRARY")
-            target_include_directories(${TARGET_NAME}
-                PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}
-            )
-        endif()
+        _get_qualifier(${TARGET_NAME}   _qualifier)
+        target_include_directories(${TARGET_NAME}
+            ${_qualifier} ${CMAKE_CURRENT_SOURCE_DIR}
+        )
     endif()
 
     if (${TARGET_NAME}_COMPILE_DEFINITIONS)
         foreach (_def IN LISTS ${TARGET_NAME}_COMPILE_DEFINITIONS)
-            target_compile_definitions(${TARGET_NAME} PRIVATE ${_def})
+            _get_qualifier(${TARGET_NAME}   _qualifier)
+            target_compile_definitions(${TARGET_NAME} ${_qualifier} ${_def})
         endforeach()
     endif()
 
@@ -56,6 +63,10 @@ function(_configure_target TARGET_NAME)
     endif()
 
     foreach(_lib IN LISTS _libraries _depends)
+        if (NOT TARGET ${_lib})
+            continue()
+        endif()
+
         get_target_property(_target_type ${_lib} TYPE)
         if(_target_type STREQUAL "INTERFACE_LIBRARY")
             get_property(_include_dirs DIRECTORY PROPERTY ${_lib}_INCLUDE_DIR)
@@ -64,6 +75,10 @@ function(_configure_target TARGET_NAME)
             endif()
             continue()
         endif()
+
+        if (_target_type STREQUAL "STATIC_LIBRARY" OR _target_type STREQUAL "MODULE_LIBRARY" OR _target_type STREQUAL "SHARED_LIBRARY")
+            target_link_libraries(${TARGET_NAME} ${_lib})
+        endif ()
 
         get_target_property(_dep_libraries ${_lib} LIBRARIES)
         if (_dep_libraries)
@@ -90,87 +105,24 @@ function(_is_kw CHECK_STR KW_LIST RESULT)
     endif()
 endfunction()
 
-function(_parse_target_args TARGET_NAME KW_LIST)
-    set(key "")
-    set(to_parent_scope FALSE)
+function(_parse_target_args TARGET_NAME KW_FLAGS KW_VALUES KW_LISTS)
+    # https://cmake.org/cmake/help/latest/command/cmake_parse_arguments.html
+    # https://stackoverflow.com/questions/23327687/how-to-write-a-cmake-function-with-more-than-one-parameter-groups
+    cmake_parse_arguments(PARSE_ARGV 1 ${TARGET_NAME}
+        "${${KW_FLAGS}}" "${${KW_VALUES}}" "${${KW_LISTS}}"
+    )
+
+    set(kw_list "${${KW_FLAGS}}" "${${KW_VALUES}}" "${${KW_LISTS}}")
     foreach(arg IN LISTS ARGN)
         # Check is 'arg' a keyword.
-        _is_kw(${arg} "${KW_LIST}" is_keyword)
+        _is_kw(${arg} kw_list is_keyword)
 
         # If 'arg' is keyword - save 'arg' to 'key' variable and save key-flag to parent scope.
         if (is_keyword)
-            if (to_parent_scope)
-                if (NOT DEFINED ${TARGET_NAME}_${key})
-                    set_property(DIRECTORY PROPERTY ${TARGET_NAME}_${key} "")
-                    set(${TARGET_NAME}_${key} TRUE)
-                endif()
-                set_property(DIRECTORY APPEND PROPERTY ${TARGET_NAME}_${key} "${${TARGET_NAME}_${key}}")
-                set(${TARGET_NAME}_${key} "${${TARGET_NAME}_${key}}" PARENT_SCOPE)
-                set(to_parent_scope FALSE)
-            endif()
-
             set(key "${arg}")
-            set(to_parent_scope TRUE)
-            continue()
-        endif()
-
-        # If 'key' variable is defined - add data to key args and add to parent scope.
-        if (key)
-            if (NOT DEFINED ${TARGET_NAME}_${key})
-                set_property(DIRECTORY PROPERTY ${TARGET_NAME}_${key} "${arg}")
-                set(${TARGET_NAME}_${key} "${arg}")
-            else()
-                set_property(DIRECTORY APPEND PROPERTY ${TARGET_NAME}_${key} "${arg}")
-                set(${TARGET_NAME}_${key} "${${TARGET_NAME}_${key}};${arg}")
-            endif()
-        endif()
-    endforeach()
-
-    if (to_parent_scope)
-        if (NOT DEFINED ${TARGET_NAME}_${key})
-            set_property(DIRECTORY PROPERTY ${TARGET_NAME}_${key} "")
-            set(${TARGET_NAME}_${key} TRUE)
-        endif()
-        get_property(_${TARGET_NAME}_${key}_prop DIRECTORY PROPERTY ${TARGET_NAME}_${key})
-        if (NOT _${TARGET_NAME}_${key}_prop)
             set_property(DIRECTORY APPEND PROPERTY ${TARGET_NAME}_${key} "${${TARGET_NAME}_${key}}")
+            set(${TARGET_NAME}_${key} "${${TARGET_NAME}_${key}}" PARENT_SCOPE)
         endif()
-        set(${TARGET_NAME}_${key} "${${TARGET_NAME}_${key}}" PARENT_SCOPE)
-        set(to_parent_scope FALSE)
-    endif()
-endfunction()
-
-macro(_validate_args TARGET_NAME KW_LIST FLAG_KW_LIST VALUES_KW_LIST)
-    foreach(key IN LISTS ${KW_LIST})
-        if (NOT DEFINED ${TARGET_NAME}_${key})
-            continue()
-        endif()
-
-        _is_kw(${key} "${FLAG_KW_LIST}" is_flag)
-        if (is_flag)
-            if ("${${TARGET_NAME}_${key}}" MATCHES "TRUE")
-                continue()
-            elseif ("${${TARGET_NAME}_${key}}" MATCHES "FALSE")
-                continue()
-            else()
-                message(FATAL_ERROR " Invalid flag value for key '${key}'")
-            endif()
-
-            continue()
-        endif()
-
-        _is_kw(${key} "${VALUES_KW_LIST}" is_value)
-        if (is_value)
-            if ("${${TARGET_NAME}_${key}}" MATCHES "TRUE")
-                message(FATAL_ERROR " Invalid value for key '${key}'")
-            elseif ("${${TARGET_NAME}_${key}}" MATCHES "FALSE")
-                message(FATAL_ERROR " Invalid value for key '${key}'")
-            endif()
-
-            continue()
-        endif()
-
-        message(ERROR " Unsupported key '${key}'")
     endforeach()
-endmacro()
+endfunction()
 
