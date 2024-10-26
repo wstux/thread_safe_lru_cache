a63ca779877c63c571f12d0d48e2b41c8570ddef
Chistyakov Alexander <wstux1@gmail.com>
2024-10-26 20:27:51 +0300

    * libs/lru: #29 Move code to another file;

--- libs/lru/details/intrusive_base_lru_cache.h
+++ libs/lru/details/intrusive_base_lru_cache.h
@@ -28,11 +28,84 @@
 #include <memory>
 #include <vector>
 
-#include "lru/details/intrusive_traits.h"
+#include <boost/intrusive/list.hpp>
+#include <boost/intrusive/unordered_set.hpp>
 
 namespace wstux {
 namespace lru {
 namespace details {
+
+namespace bi = boost::intrusive;
+
+typedef bi::link_mode<bi::normal_link>          link_mode;
+typedef bi::list_base_hook<link_mode>           list_hook;
+typedef bi::unordered_set_base_hook<link_mode>  hash_tbl_hook;
+
+template<typename TKey, typename TValue>
+struct lru_node : public list_hook
+                , public hash_tbl_hook
+{
+    typedef TKey        key_type;
+    typedef TValue      value_type;
+
+    lru_node(key_type&& k, value_type&& v)
+        : key(std::move(k))
+        , value(std::move(v))
+    {}
+
+    key_type key;
+    value_type value;
+};
+
+template<typename THash>
+struct lru_node_hash
+{
+    template<typename T>
+    size_t operator()(const T& t) const { return THash{}(t); }
+
+    template<typename TKey, typename TValue>
+    size_t operator()(const lru_node<TKey, TValue>& n) const { return THash{}(n.key); }
+};
+
+template<typename TKey>
+const TKey& _key(const TKey& k) { return k; }
+
+template<typename TKey, typename TValue>
+const TKey& _key(const lru_node<TKey, TValue>& n) { return n.key; }
+
+template<typename TKeyEqual>
+struct lru_node_equal
+{
+    template<typename T1, typename T2>
+    bool operator()(const T1& l, const T2& r) const { return TKeyEqual{}(_key(l), _key(r)); }
+};
+
+template<typename TKey, typename TValue, typename THash, typename TKeyEqual>
+struct intrusive_traits
+{
+    typedef TKey                key_type;
+    typedef TValue              value_type;
+    typedef value_type&         reference;
+    typedef const value_type&   const_reference;
+    typedef value_type*         pointer;
+    typedef const value_type*   const_pointer;
+
+    typedef size_t              size_type;
+    typedef THash               hasher;
+    typedef TKeyEqual           key_equal;
+
+    typedef lru_node<key_type, value_type>  _lru_node;
+    typedef bi::list<_lru_node, bi::constant_time_size<false>>  lru_list;
+
+    //typedef value_type          hash_table_value;
+    typedef bi::constant_time_size<true>            _is_ct_size;
+    typedef bi::hash<lru_node_hash<hasher>>         _intrusive_hash;
+    typedef bi::equal<lru_node_equal<key_equal>>    _intrusive_key_equal;
+    typedef bi::unordered_set<_lru_node, _is_ct_size, _intrusive_hash, _intrusive_key_equal> hash_table;
+
+    typedef typename hash_table::bucket_type    _bucket_type;
+    typedef typename hash_table::bucket_traits  _bucket_traits;
+};
 
 /**
  *  @brief  Implementation of lru_cache based on intrusive containers.
